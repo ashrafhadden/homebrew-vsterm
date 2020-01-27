388736fa07e296ba24fc0f507dac60bfb2d27117
Ashraf Hadden <ashrafhadden@gmail.com>
2020-01-27 01:50:56 -0500

    Add homebrew private cask

---
+++ Casks/vsterm.rb
@@ -0,0 +1,25 @@
+cask 'vsterm' do
+  version '1.41.1'
+  sha256 '39c9a501ad9d54835e5dd2a3e336f5487edd2050ced6bb05e0dfebeb71827e77'
+
+  url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium-darwin-#{version}.zip"
+  appcast 'https://github.com/VSCodium/vscodium/releases.atom'
+  name 'VSCodium'
+  homepage 'https://github.com/VSCodium/vscodium'
+
+  auto_updates true
+  conflicts_with cask: 'visual-studio-code'
+
+  app 'VSCodium.app'
+  binary "#{appdir}/VSCodium.app/Contents/Resources/app/bin/code"
+
+  zap trash: [
+               '~/Library/Application Support/VSCodium',
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.visualstudio.code.oss.sfl*',
+               '~/Library/Logs/VSCodium',
+               '~/Library/Preferences/com.visualstudio.code.oss.helper.plist',
+               '~/Library/Preferences/com.visualstudio.code.oss.plist',
+               '~/Library/Saved Application State/com.visualstudio.code.oss.savedState',
+               '~/.vscode-oss',
+             ]
+end
